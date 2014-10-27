e59f8f77ff09e601d202f60ad66808e23b1f69a9
Chad Hietala <chadhietala@gmail.com>
2014-10-27 11:08:56 -0700

    init

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var hbsCompile = require('broccoli-ember-hbs-template-compiler');
+
 module.exports = {
-  name: 'ember-cli-hbs-template-compiler'
+  name: 'ember-cli-hbs-template-compiler',
+
+  included: function ( app ) {
+    this._super.included.apply( this, arguments );
+    app.registry.add('template', {
+      name: 'ember-cli-hbs-template-compiler',
+      ext: ['hbs', 'handlebars'],
+      toTree: function (tree) {
+        return hbsCompile(tree);
+      }
+    });
+  }
 };
