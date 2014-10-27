2c1c47fc6a0d12c70741ed1aa84044261a590654
Chad Hietala <chadhietala@gmail.com>
2014-10-27 11:46:20 -0700

    Adding options

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var hbsCompile = require('broccoli-ember-hbs-template-compiler');
+var merge = require('lodash-node/modern/objects/merge');
+
+var defaultOptions = {
+  module: true
+};
 
 module.exports = {
   name: 'ember-cli-hbs-template-compiler',
 
-  included: function ( app ) {
-    this._super.included.apply( this, arguments );
+  included: function (app) {
+    this._super.included.apply(this, arguments);
     app.registry.add('template', {
       name: 'ember-cli-hbs-template-compiler',
       ext: ['hbs', 'handlebars'],
       toTree: function (tree) {
-        return hbsCompile(tree);
+        var options = merge({}, defaultOptions, app.options.handlebars);
+        return hbsCompile(tree, options);
       }
     });
   }
